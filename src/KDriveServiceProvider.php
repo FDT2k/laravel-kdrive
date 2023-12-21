30d853ea09065d326206d2c33f5113f2fbce59b7
Serge Ledig <contact@cotiga.fr>
2023-12-21 09:31:04 +0100

    Laravel 9, 10 compatible

--- src/KDriveServiceProvider.php
+++ src/KDriveServiceProvider.php
@@ -5,6 +5,7 @@ namespace Infomaniak\KDrive;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\ServiceProvider;
 use League\Flysystem\Filesystem;
+use Illuminate\Filesystem\FilesystemAdapter;
 
 class KDriveServiceProvider extends ServiceProvider
 {
@@ -14,13 +15,17 @@ class KDriveServiceProvider extends ServiceProvider
     public function boot()
     {
         Storage::extend('kdrive', function ($app, $config) {
-            $kdrive = new KDriveAdapter(
+            $adapter = new KDriveAdapter(
                 $config['id'],
                 $config['username'],
                 $config['password'],
                 $config['prefix']
             );
-            return new Filesystem($kdrive);
+            return new FilesystemAdapter(
+                new Filesystem($adapter, $config),
+                $adapter,
+                $config
+            );
         });
     }
 }
